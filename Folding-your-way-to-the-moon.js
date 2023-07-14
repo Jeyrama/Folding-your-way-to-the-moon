12fb5b789f43b3c49852f599ab063c4f2639ea6b
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-14 12:17:14 -0700

    Update Folding-your-way-to-the-moon.js

--- Folding-your-way-to-the-moon.js
+++ Folding-your-way-to-the-moon.js
@@ -30,3 +30,11 @@ function foldTo(distance) {
 }
 
 // or
+
+const THICKNESS = 0.0001;
+
+function foldTo(distance) {
+  if (distance < 0) { return null; }
+  if (distance < THICKNESS) { return 0; }
+  return Math.ceil(Math.log2(distance/THICKNESS));
+}
